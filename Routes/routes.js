dbd623647b9226dae912c594cf247c3a11162e11
Axel <axelbrostedt@gmail.com>
2019-10-30 20:56:45 -0600

    Skeletons for routes and express server. Also made directories for models, controllers and public

---
+++ Routes/routes.js
@@ -0,0 +1,25 @@
+let router = require('express').Router();
+
+
+// Get all reviews for one product
+router.get('/reviews/:product_id/list', (req, res, next) => {
+
+})
+// get meta data (ratings / characteristics) for one product
+router.get('/reviews/:product_id/meta', (req, res, next) => {
+
+})
+// post for adding a new review
+router.post('/reviews/:product_id', (req, res, next) => {
+
+})
+// Mark a review as helpful
+router.put('/reviews/helpful/:review_id', (req, res, next) => {
+
+})
+// Report a review
+router.put('/reviews/report/:review_id', (req, res, next) => {
+
+})
+
+module.exports = request;
