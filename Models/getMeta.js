0006e5778db862137892201cc8793934dfb2aeaf
Axel <axelbrostedt@gmail.com>
2019-11-04 12:33:25 -0700

    Adds query for getting characteristics' names and assigns them to the response object successfully

--- Models/getMeta.js
+++ Models/getMeta.js
@@ -47,7 +47,7 @@ const getMeta = async function(product_id) {
         "characteristics": {}
     }
     let reviewsQuery = await db.query(`SELECT * from reviews WHERE product_id = ${product_id}`);
-    let rows = reviewsQuery.rows;
+    let reviewsRows = reviewsQuery.rows;
     let ratingsObj = {
         1: 0,
         2: 0,
@@ -59,28 +59,51 @@ const getMeta = async function(product_id) {
         0: 0,
         1: 0
     }
-    for (let i = 0; i < rows.length; i++) {
-        if (rows[i].rating === 1) {
+    for (let i = 0; i < reviewsRows.length; i++) {
+        if (reviewsRows[i].rating === 1) {
             ratingsObj[1] += 1;
-        } else if (rows[i].rating === 2) {
+        } else if (reviewsRows[i].rating === 2) {
             ratingsObj[2] += 1;
-        } else if (rows[i].rating === 3) {
+        } else if (reviewsRows[i].rating === 3) {
             ratingsObj[3] += 1;
-        } else if (rows[i].rating === 4) {
+        } else if (reviewsRows[i].rating === 4) {
             ratingsObj[4] += 1;
-        } else if (rows[i].rating === 5) {
+        } else if (reviewsRows[i].rating === 5) {
             ratingsObj[5] += 1;
         }
 
-        if (rows[i].recommend === false) {
+        if (reviewsRows[i].recommend === false) {
             recommendedObj[0] += 1;
-        } else if (rows[i].recommend === true) {
+        } else if (reviewsRows[i].recommend === true) {
             recommendedObj[1] += 1;
         }
     }
     response.ratings = ratingsObj;
     response.recommended = recommendedObj;
+
+    const characteristicsQuery = await db.query(`SELECT * from characteristics WHERE product_id = ${product_id}`);
+    let charRows = characteristicsQuery.rows;
+    let characteristicObj = {};
+    
+    for (let i = 0; i < charRows.length; i++) {
+        let nameObj = {
+            "id": 0,
+            "value": 0
+        }
+        characteristicObj[charRows[i].name] = {};
+        //const valueQuery = await db.query(`SELECT * from characteristics_reviews WHERE review_id = ${charRows[i].id}`)
+    }
+    response.characteristics = characteristicObj;
     return response;
 }
 
 module.exports = { getMeta };
+
+// Make query to characteristics table, X
+    // loop through the response rows X
+    // create new characteristicsObj, with rows[i].name as the name, X
+        // make a new query to characteristics_reviews table using rows[i].id 
+            // create a new nameObj
+            // loop through response rows,
+            // set id to rows[i].characteristic_id and value to rows[i].value
+        // append nameObj to characteristicObj
